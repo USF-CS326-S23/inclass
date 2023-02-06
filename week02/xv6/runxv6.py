46bbbe29b184e1c3fa5ca2842d763ffd9bb836a2
Greg Benson <benson@cs.usfca.edu>
2023-02-06 15:26:46 -0800

    Updated runxv6.py timeout to 5 minutes.

--- week02/xv6/runxv6.py
+++ week02/xv6/runxv6.py
@@ -1,12 +1,23 @@
+import atexit
 import io
 import pexpect
 import sys
+
+child = None
+
+def cleanup():
+    if child:
+        #print("xv6run: cleanup")
+        child.close(force=True)
+
+
+atexit.register(cleanup)
 
 if len(sys.argv) < 2:
     print("Provide a command to execute.")
     sys.exit(-1)
 
-child = pexpect.spawn('make qemu', encoding='utf-8')
+child = pexpect.spawn('make qemu', encoding='utf-8', timeout=300)
 #child.logfile_read = sys.stdout
 child.expect_exact('$ ')
 child.sendline(sys.argv[1])
